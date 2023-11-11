9eb05f4e6b3f6d5d3a6c830d3d01657b32313d41
Nicolas Gonsalves <nicolas.gjdp@gmail.com>
2023-11-11 18:25:18 +0100

    Home, Over Ons & Contact page text changes

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bakery Login</title>
-  <!-- Include FontAwesome CSS (you can get it from https://fontawesome.com/) -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="login.css">
 </head>
@@ -15,7 +14,7 @@
     <img src="images/bakkery-login-image.jpg" alt="Bakery Logo">
   </div>
   <h2>Bakery Login</h2>
-  <form action="login_process.php" method="post"> <!-- Specify the form action and method -->
+  <form action="login_process.php" method="post">
     <div class="input-group">
       <i class="fas fa-envelope"></i>
       <label for="email">Email:</label>
