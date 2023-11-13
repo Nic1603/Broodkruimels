0dc90bcbf1e5c1f849510f4ee02eb9f57711a112
Nicolas Gonsalves <nicolas.gjdp@gmail.com>
2023-11-13 01:27:39 +0100

    Aanpassing aan contact telefoonnummer en css

--- ayoub code/ayoub_script.js
+++ ayoub code/ayoub_script.js
@@ -1,15 +1,15 @@
 const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
+const userCardContainer = document.querySelector("[product]")
 const searchInput = document.querySelector("[data-search]")
  
-let users = []
+let product = []
  
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
     const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
+      product.toLowerCase().includes(value) ||
+      product.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisible)
   })
 })
@@ -17,14 +17,14 @@ searchInput.addEventListener("input", e => {
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
   .then(data => {
-    users = data.map(user => {
+    product = data.map(product => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
-      header.textContent = user.name
-      body.textContent = user.email
+      header.textContent = product
+      body.textContent = product
       userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+      return { name: product.name, email: product.email, element: card }
     })
   })
 
